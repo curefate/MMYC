5495bc8999a1f1b9c06efe7919f17b1d4deb7ce7
curefate <66023115+curefate@users.noreply.github.com>
2026-05-05 19:56:54 +0200

    fix: offset of minimap

--- Assets/Workspace/Scripts/Minimap.cs
+++ Assets/Workspace/Scripts/Minimap.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Minimap : NetworkBehaviour
 {
     [Networked]
-    public string CurrentRoomID { get; set; }
+    public string CurrentRoomID { get; set; } = string.Empty;
 
     private Dictionary<string, Room> roomDict;
     private Room currentRoom = null;
@@ -27,8 +27,14 @@ public class Minimap : NetworkBehaviour
         }
     }
 
-    private void SwitchToRoom(string roomID)
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public void Rpc_SwitchToRoom(string roomID)
     {
+        if (!Object.HasStateAuthority)
+            return;
+
+        if (roomID == CurrentRoomID) return;
+
         if (roomDict.TryGetValue(roomID, out Room newRoom))
         {
             if (newRoom == currentRoom) return;
@@ -43,6 +49,19 @@ public class Minimap : NetworkBehaviour
         else
         {
             Debug.LogError($"Room with ID {roomID} not found in Minimap.");
+        }
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+    public void Rpc_LockAllRooms()
+    {
+        if (!Object.HasStateAuthority)
+            return;
+
+        foreach (var room in roomDict.Values)
+        {
+            if (room.RoomID == currentRoom.RoomID) return;
+            room.IsAccessible = false;
         }
     }
 }
--- Assets/Workspace/Scripts/Room.cs
+++ Assets/Workspace/Scripts/Room.cs
@@ -1,14 +1,55 @@
 using System.Collections.Generic;
 using Fusion;
+using Oculus.Interaction;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Room : MonoBehaviour
+public class Room : NetworkBehaviour
 {
-    public string RoomID;
-    public GameObject RoomObj;
+    [Networked]
+    public string RoomID { get; set; } = string.Empty;
+    [Networked]
+    public bool IsAccessible { get; set; } = false;
+
     public List<Room> NeighborRooms;
 
+    public GameObject Visual;
+    public InteractableUnityEventWrapper Wrapper;
+
+    public UnityEvent OnFinishRoom;
     public UnityEvent OnEnterRoom;
     public UnityEvent OnExitRoom;
+
+    private Vector3 originalVisualPos;
+
+    void Awake()
+    {
+        if (NeighborRooms == null || NeighborRooms.Count == 0) Debug.LogWarning($"Room {RoomID} has no neighboring rooms assigned.");
+
+        Minimap minimap = FindFirstObjectByType<Minimap>();
+        if (minimap != null)
+        {
+            minimap.RegisterRoom(this);
+        }
+        else
+        {
+            Debug.LogWarning($"No Minimap found for Room {RoomID}.");
+        }
+
+        originalVisualPos = Visual.transform.localPosition;
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (IsAccessible)
+        {
+            Visual.transform.localPosition = originalVisualPos;
+            Wrapper.enabled = true;
+        }
+        else
+        {
+            Visual.transform.localPosition = Vector3.zero;
+            Wrapper.enabled = false;
+        }
+    }
 }
