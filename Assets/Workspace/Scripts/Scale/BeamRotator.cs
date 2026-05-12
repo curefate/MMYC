618e72fda808b66524052d3e05afb811df669b7e
curefate <66023115+curefate@users.noreply.github.com>
2026-05-12 21:04:08 +0200

    fix: scale sample done

--- Assets/Workspace/Scripts/Scale/BeamRotator.cs
+++ Assets/Workspace/Scripts/Scale/BeamRotator.cs
@@ -6,25 +6,40 @@ public class BeamRotator : NetworkBehaviour
     public int Speed = 10;
     public float MaxAngle = 25f;
     public float MaxWeightDifference = 7f;
+
     [Networked]
     public float Angle { get; private set; }
 
+    private float visualAngle;
     private float anglePerWeight => MaxAngle / MaxWeightDifference;
 
     public override void FixedUpdateNetwork()
     {
-        float targetAngle = Mathf.Lerp(transform.localEulerAngles.z, Angle, Speed * Runner.DeltaTime);
-        transform.localRotation = Quaternion.Euler(0, 0, targetAngle);
-
         if (!Object.HasStateAuthority)
             return;
 
         UpdateAngle();
     }
 
+    public override void Render()
+    {
+        visualAngle = Mathf.Lerp(visualAngle, Angle, Speed * Time.deltaTime);
+        transform.localRotation = Quaternion.Euler(0, 0, visualAngle);
+    }
+
     private void UpdateAngle()
     {
         if (!Object.HasStateAuthority)
             return;
+
+        float l0 = ScaleDef.GetWeightBySerial(MQTTProcessor.Instance.Hall_0);
+        float l1 = ScaleDef.GetWeightBySerial(MQTTProcessor.Instance.Hall_1);
+        float r0 = ScaleDef.GetWeightBySerial(MQTTProcessor.Instance.Hall_2);
+        float r1 = ScaleDef.GetWeightBySerial(MQTTProcessor.Instance.Hall_3);
+        float weight_l = l0 + l1 + ScaleDef.LeftDefaultWeight;
+        float weight_r = r0 + r1 + ScaleDef.RightDefaultWeight;
+
+        Angle = Mathf.Clamp((weight_l - weight_r) * anglePerWeight, -MaxAngle, MaxAngle);
+        Debug.Log($"Left: {weight_l}, Right: {weight_r}, Angle: {Angle}");
     }
 }
--- Assets/Workspace/Scripts/Scale/ScaleDef.cs
+++ Assets/Workspace/Scripts/Scale/ScaleDef.cs
@@ -12,7 +12,7 @@ public static class ScaleDef
         Eye
     }
 
-    public static Dictionary<WeightType, float> Weight = new()
+    public static Dictionary<WeightType, float> WeightDict = new()
     {
         { WeightType.None, 0f },
         { WeightType.Skull, 3.5f },
@@ -26,13 +26,22 @@ public static class ScaleDef
 
     public static WeightType GetWeightTypeBySerial(int num)
     {
-        throw new System.NotImplementedException();
-        // TODO
+        if (num <= 3000)
+            return WeightType.Eye;
+        else if (num > 3000 && num <= 4800)
+            return WeightType.Cat;
+        else if (num > 4800 && num <= 5800)
+            return WeightType.None;
+        else if (num > 5800 && num <= 7500)
+            return WeightType.Skull;
+        else if (num >= 7500)
+            return WeightType.Pyramid;
+        else
+            return WeightType.None;
     }
 
     public static float GetWeightBySerial(int num)
     {
-        throw new System.NotImplementedException();
-        // TODO
+        return WeightDict[GetWeightTypeBySerial(num)];
     }
 }
