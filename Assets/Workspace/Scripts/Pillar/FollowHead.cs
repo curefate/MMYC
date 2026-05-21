d3a651838c7561288af3cfc9cd7ff25f0ac781b0
curefate <66023115+curefate@users.noreply.github.com>
2026-05-21 16:14:19 +0200

    add: import pillar asset, merge test

--- Assets/Workspace/Scripts/Pillar/FollowHead.cs
+++ Assets/Workspace/Scripts/Pillar/FollowHead.cs
@@ -1,13 +1,21 @@
 using Fusion;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FollowHead : NetworkBehaviour
 {
     public Vector3 offset;
+    public UnityEvent OnBackToPosition;
 
     public override void FixedUpdateNetwork()
     {
         if (!Object.HasStateAuthority) return;
         transform.position = Camera.main.transform.position + offset;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!Object.HasStateAuthority) return;
+        OnBackToPosition?.Invoke();
     }
 }
